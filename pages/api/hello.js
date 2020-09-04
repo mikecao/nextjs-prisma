0ee876986fa404d5f19c3ae97cf8051147ae20cb
Mike Cao <mike@mikecao.com>
2020-09-03 22:36:20 -0700

    Initial commit.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
+
+async function runQuery(query) {
+  return query
+    .catch(e => {
+      throw e;
+    })
+    .finally(async () => {
+      // This causes the error
+      await prisma.$disconnect();
+    });
+}
+
+async function testQuery() {
+  return runQuery(prisma.$queryRaw('select now()'));
+}
+
+export default async (req, res) => {
+  const promises = [];
+
+  for (let i = 0; i < 10; i++) {
+    promises.push(testQuery());
+  }
+
+  const result = await Promise.all(promises);
+
+  res.statusCode = 200;
+  res.json({ result });
 }
